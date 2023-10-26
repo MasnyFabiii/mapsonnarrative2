596f1dc7f0d1a7cc5679d000037103b91957c54d
MasnyFabiii <149096909+MasnyFabiii@users.noreply.github.com>
2023-10-26 19:09:10 +0200

    google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRPkuTx4UaH-hGfzjEzzOWSy6ESKnLD4gx_b3kivVLrZoC__FcmjI6O0gL744fM75SaTs2y_xDERRTH/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1iQMUDlKwEGkkE4I2-nRNRaT2xTjgoGKabmyLSF_VNsE/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyArfX2zRdtj_9WzWvKahc6WvzwHOKrnmqI';
